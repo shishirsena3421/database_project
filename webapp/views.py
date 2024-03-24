a770751918c28a9aa283af5689c1d79d6671dfce
Shishir Timilsena <69421280+shishirsena3421@users.noreply.github.com>
2024-03-24 18:19:51 -0400

    Add files via upload

---
+++ webapp/views.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render, redirect
+
+from .forms import CreateUserForm, LoginForm 
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+
+from django.contrib.auth.decorators import login_required
+
+from .models import Record
+
+
+def home(request):
+
+    return render(request, 'webapp/index.html')
+
+
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("my-login")
+
+    context = {'form':form}
+    return render(request, 'webapp/register.html', context=context)
+
+
+
+# login a user
+
+def my_login(request):
+
+    form = LoginForm()
+
+    if request.method == "POST":
+
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+
+                return redirect("dashboard")
+
+                
+
+    context = {'form':form} 
+
+    return render(request, 'webapp/my-login.html', context=context)           
+
+
+
+
+
+# DASHBOARD
+
+
+@login_required(login_url='my-login')
+
+def dashboard(request):
+
+    my_records = Record.objects.all()
+
+    context = {'records': my_records}
+
+    return render(request, 'webapp/dashboard.html', context=context)
+
+
+
+
+
+
+
+
+# logout a user
+
+
+
+def user_logout(request):
+
+    auth.logout(request)
+
+    return redirect("my-login")
+
